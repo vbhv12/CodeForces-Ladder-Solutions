4dc644bb29a4ce57f75e2c5b09e7fe08eaa9c9f6
vbhv12 <48524857+vbhv12@users.noreply.github.com>
2020-10-06 21:33:22 +0530

    Update 9-soft_drinking.cpp

--- Ladder A/9-soft_drinking.cpp
+++ Ladder A/9-soft_drinking.cpp
@@ -2,11 +2,13 @@
 using namespace std;
 
 int main() {
+	//required variables
 	int n, k, l, c, d, p, nl, np;
 	cin>>n>>k>>l>>c>>d>>p>>nl>>np;
 	int total_ml = k*l, total_slices=c*d;
 	int condition1=(total_ml/nl);
 	int condition2=total_slices;
 	int condition3=p/np;
+	//outputting the min
 	cout<<min(min(condition1, condition2), condition3)/n;	
 }
